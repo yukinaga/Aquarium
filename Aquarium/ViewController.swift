c4f35510cee1c8783a729114e1e744cd36d19e2e
yuky_az <yuky.azuma@gmail.com>
2014-09-20 16:24:55 +0900

    Initial Commit

---
+++ Aquarium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aquarium
+//
+//  Created by Yukinaga Azuma on 2014/09/20.
+//  Copyright (c) 2014年 Yukinaga Azuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
